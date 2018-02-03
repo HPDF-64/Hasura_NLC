1738b954d9af7dc3ec36752d18d7856d2d5206ff
Sachin <hellosachindev@gmail.com>
2018-02-03 23:39:49 +0530

    Fetching From Web and Display Done

--- React_Native/App.js
+++ React_Native/App.js
@@ -14,19 +14,89 @@ import {
   TextInput,
   Button,
   ScrollView,
+  Alert,PermissionsAndroid,
 } from 'react-native';
 import { TextField } from 'react-native-material-textfield';
 
 export default class App extends Component<{}> {
 
+  async _Fetchdata (){
+    return fetch('http://139.59.67.180/hpdf.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        //results= responseJson.categories;
+        // console.log(results);
+        // var data = results.map(function(item) {
+        //   console.log(item.label);
+        //   console.log(item.score);
+        // });
+        p = JSON.parse(JSON.stringify(responseJson.categories));
+        return p;
+    //  console.log(responseJson.categories.label)
+      })
+
+      .catch((error) => {
+        console.error(error);
+      });
+
+  }
+  async _Analyse(){
+    this._Fetchdata();
+    this.setState({showTheThing:true});
+      console.log("Clicked");
+      for (i=0;i<3;i++){
+        console.log(p[i].label);
+      }
+      var Row1Array = p[0].label.split('/');
+      var Row2Array = p[1].label.split('/');
+      var Row3Array = p[2].label.split('/');
+      console.log(Row1Array[1]);
+      this.setState({v1:p[0].score*100+"%"});
+      this.setState({v2:p[1].score*100+"%"});
+      this.setState({v3:p[2].score*100+"%"});
+
+      this.setState({h1:Row1Array[1]});
+      this.setState({h2:Row2Array[1]});
+      this.setState({h3:Row3Array[1]});
+
+      this.setState({sh1:Row1Array[2]});
+      this.setState({sh2:Row2Array[2]});
+      this.setState({sh3:Row3Array[2]});
+
+      this.setState({mh1:Row1Array[3]});
+      this.setState({mh2:Row2Array[3]});
+      this.setState({mh3:Row3Array[3]});
+   //  if(this.state.showTheThing){
+   //   this.setState({showTheThing:false});
+   // }else{
+   //   this.setState({showTheThing:true});
+   // }
+
+}
+
   constructor(props){
    super(props);
+   this._Analyse=this._Analyse.bind(this);
    this.state = {
-      text: "TextInput \n\n\n\nA foundational component for inputting text into the app via a keyboard. Props provide configurability for several features, such as auto-correction, auto-capitalization, placeholder text, and different keyboard types, such as a numeric keypad. The simplest use case is to plop down a TextInput and subscribe to the onChangeText events to read the user input. There are also other events, such as onSubmitEditing and onFocus that can be subscribed to. A simple example:"
+      text: "TextInput \n\n\n\nA foundational component for inputting text into the app via a keyboard. Props provide configurability for several features, such as auto-correction, auto-capitalization, placeholder text, and different keyboard types, such as a numeric keypad. The simplest use case is to plop down a TextInput and subscribe to the onChangeText events to read the user input. There are also other events, such as onSubmitEditing and onFocus that can be subscribed to. A simple example:",
+      v1:0,
+      v2:0,
+      v3:0,
+      h1:"Heading1",
+      h2:"Heading2",
+      h3:"Heading3",
+      sh3:"Sub Heading3",
+      sh2:"Sub Heading2",
+      sh1:"Sub Heading1",
+      mh1:"Mini Heading1",
+      mh2:"Mini Heading2",
+      mh3:"Mini Heading3",
+      headingFull:["","",""],
+
    }
 }
   render() {
-    let { errors = {}, secureTextEntry, ...data } = this.state;
+    let { errors = {},v1,v2,v3, h1, h2,h3,sh1,sh2,sh3,mh1,mh2,mh3,ht1,ht2,ht3,headingFull, p, secureTextEntry, ...data } = this.state;
     return (
       <ScrollView contentContainerStyle={styles.contentContainer}>
       <View style={styles.container}>
@@ -55,55 +125,72 @@ export default class App extends Component<{}> {
           style={styles.buttonGradient,styles.buttonText}
 
             title="Analyze"
-            color="#fff"
-            accessibilityLabel="Learn more about this purple button"
+            onPress={this._Analyse}
+          //  color="#fff"
           />
           </LinearGradient>
-          <View style={styles.graph}>
-              <LinearGradient colors={['#F23673', '#FFC066']} style={styles.linearGradient1}/>
-              <LinearGradient colors={['#009FC5', '#3CECB0']} style={styles.linearGradient2}/>
-              <LinearGradient colors={['#AD2AB9', '#FF618C']} style={styles.linearGradient3}/>
-          </View>
-          <View style={styles.graphText}>
-          <View style={styles.textContainer}>
-          <Text style={styles.OutputText1}> Databases</Text>
-          </View>
-          <View style={styles.textContainer}>
-          <Text style={styles.OutputText1}> Java</Text>
-          </View>
-          <View style={styles.textContainer}>
-          <Text style={styles.OutputText1}> Headaches And Migranes</Text>
-          </View>
-          </View>
-          <View style={styles.graphText}>
-          <View style={styles.textContainer}>
-          <Text style={styles.OutputText2}>Software</Text>
-          </View>
-          <View style={styles.textContainer}>
-          <Text style={styles.OutputText2}>Programming Language</Text>
-          </View>
-          <View style={styles.textContainer}>
-          <Text style={styles.OutputText2}>Disease</Text>
-          </View>
-          </View>
+          { this.state.showTheThing &&
+
+            <View>
+                    <View style={styles.graph}>
+                        <LinearGradient colors={['#F23673', '#FFC066']} style={styles.linearGradient1}/>
+                        <LinearGradient colors={['#009FC5', '#3CECB0']} style={styles.linearGradient2}/>
+                        <LinearGradient colors={['#AD2AB9', '#FF618C']} style={styles.linearGradient3}/>
+                    </View>
+                    <View style={styles.graphText}>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.OutputText1}> {v1}</Text>
+                        </View>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.OutputText1}> {v2}</Text>
+                        </View>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.OutputText1}> {v3}</Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.graphText}>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.OutputText1}> {h1}</Text>
+                        </View>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.OutputText1}> {h2}</Text>
+                        </View>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.OutputText1}> {h3}</Text>
+                        </View>
+                    </View>
+                    <View style={styles.graphText}>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.OutputText2}>{sh1}</Text>
+                        </View>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.OutputText2}>{sh2}</Text>
+                        </View>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.OutputText2}>{sh3}</Text>
+                        </View>
+                    </View>
 
 
 
-          <View style={styles.graphText}>
-          <View style={styles.textContainer}>
-          <Text style={styles.OutputText3}>Technology and Computing</Text>
-          </View>
-          <View style={styles.textContainer}>
-          <Text style={styles.OutputText3}>Technology and Computing</Text>
-          </View>
+                    <View style={styles.graphText}>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.OutputText3}>{mh1}</Text>
+                        </View>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.OutputText3}>{mh2}</Text>
+                        </View>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.OutputText3}>{mh3}</Text>
+                        </View>
+                    </View>
 
-          <View style={styles.textContainer}>
-          <Text style={styles.OutputText3}>Health and Fitness</Text>
-          </View>
-          </View>
 
 
-      </View>
+         </View>
+    }
+    </View>
       </ScrollView>
     );
   }
